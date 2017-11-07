c83ef32020c92279ccd514e237ce4a59ec06cade
WillForan <willforan@gmail.com>
2017-11-07 15:49:46 -0500

    fix issues w 10894_20111117(#input), 10912+10934+10972 (no ec)

--- 01.2_eyefile.R
+++ 01.2_eyefile.R
@@ -5,26 +5,39 @@ library(gdata) # read xls (not xlsx)
 library(tidyr)
 options(warn=1) # warn as it happens
 
+## 20171106 - WF - OR
+# 10894_20111117 # input issue: too many match files
+# 10912_20120104 # column issue
+# 10934_20120207 
+# 10972_20120417
 
 ## read in manually scored eye tracking
 
+# find all scored files for a subject
 subjfiles <- function(ld) {
    ld <- gsub('_','/',ld)
    files <- Sys.glob(sprintf('/Volumes/L/bea_res/Data/Tasks/CogEmoSoundsBasic/%s/Scored/*/fs_*xls',ld))
    files <- files[!grepl('OLD',files,ignore.case = T)]
+   if(length(files)>4) files <- files[!grepl('fs_[^0-9]',files,ignore.case = T)]
    return(files)
 }
+# read in the final score ('fs') spread sheet
+# expect 9 rows:
+#  score lat1 lat2 lat3 valence     score.val trial      score.ec lat.ec
+# but if no errors were made or none correctd, the last column (8, lat.ec) will not exist
 readfs <- function(f) { 
     fn=basename(f)
-    d<-read.xls(f,sheet=2,header=F)%>%mutate(run=fn) 
+    d<-read.xls(f,sheet=2,header=F)
 
     # if we never had an error correct, the last column will never be populated
-    if(nrow(d) == 9) d$errcorlat <- NA
+    if(ncol(d) == 8) d[,9] <- NA
 
-    if(nrow(d) < 0 || ncol(d) != 10) warning(f, ' has too few rows or columns!')
+    if(nrow(d) < 0 || ncol(d) != 9) warning(f, ' has too few rows or columns! ',nrow(d), ' and ', ncol(d))
 
-    return(d)
+    return(d%>%mutate(run=fn) )
 }
+
+# merge all the final manually scored sheets together (long)
 readallfseye<- function(ld) {
    files<-subjfiles(ld)
    d <- lapply(files, readfs) %>%
